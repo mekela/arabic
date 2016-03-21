360f0e0854526d0ddb4d28ca934035e4213d158a
alex <sania_free@i.ua>
2016-03-21 17:44:16 +0200

    first page

---
+++ js/common.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	//fancybox
+	$('.fancybox').fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	
+	//bxslider
+	$('.slider_wrap ul').bxSlider({
+	    slideWidth: 333,
+	    minSlides: 2,
+	    maxSlides: 3,
+	    slideMargin: 1
+	});
+
+	//menu
+	$( ".menu_trigger" ).click(function() {
+	  $( this ).next("ul").toggle( "slow");
+	});
+});
